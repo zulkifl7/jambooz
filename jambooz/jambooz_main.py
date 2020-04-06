273e0b4022d966fa1816c04adc31edd46fe83734
abdulla zulkifl <abdullazulkifl7@gmail.com>
2020-04-06 04:49:43 -0400

    Update jambooz_main.py

--- jambooz/jambooz_main.py
+++ jambooz/jambooz_main.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'Jambooz-main .ui'
-#
-# Created by: PyQt5 UI code generator 5.14.1
-#
-# WARNING! All changes made in this file will be lost!
-
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
